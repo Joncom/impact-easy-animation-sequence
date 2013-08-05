3ddb872f641fc2c8378b4d87eb53e4cdbad71998
Jonathan Commins <joncom@gmail.com>
2013-08-05 00:58:30 -0700

    Renamed variables.

--- animation.js
+++ animation.js
@@ -15,16 +15,16 @@ ig.module('plugins.joncom.easy-animation-sequence.animation')
         // URL: http://impactjs.com/forums/impact-engine/little-update-to-addanim/page/1
         _convertSequenceStringToArray: function(string) {
             var sequence = [];
-            var parts = string.split(",");
-            for(var i=0; i<parts.length; i++) {
-                var part = parts[i];
-                var index = part.indexOf("-");
+            var chunks = string.split(",");
+            for(var i=0; i<chunks.length; i++) {
+                var chunk = chunks[i];
+                var index = chunk.indexOf("-");
                 if(index === -1) {
-                    var frame = parseInt(part, 10);
+                    var frame = parseInt(chunk, 10);
                     sequence.push(frame);
                 } else {
-                    var a = parseInt(part.substring(0, index), 10);
-                    var b = parseInt(part.substr(index + 1), 10);
+                    var a = parseInt(chunk.substring(0, index), 10);
+                    var b = parseInt(chunk.substr(index + 1), 10);
                     if(a < b) {
                         for (var frame = a; frame <= b; frame++) {
                             sequence.push(frame);
